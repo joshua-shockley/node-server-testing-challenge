d1ab5310501cebe578715a72ea793ae87de6286c
Joshua-Shockley <thelumbashockley@gmail.com>
2019-10-17 22:11:15 -0500

    got a bunch of passing tests there for ya

--- api/server.spec.js
+++ api/server.spec.js
@@ -30,3 +30,46 @@ describe('GET /', () => {
             })
     })
 });
+
+describe('get /api/persons', () => {
+    it('should get a 200 upon success', () => {
+        request(server).get('/api/persons')
+            .then(response => {
+                expect(response.status).toBe(200);
+            })
+    })
+    it('should return json response type', () => {
+        request(server).get('/api/persons')
+            .then(response => {
+                expect(response.type).toMatch(/json/i);
+            })
+    })
+})
+
+describe('post /', () => {
+    it('should return 200 status code', () => {
+        console.time();
+
+        return request(server)
+            .post('/api/persons')
+            .send({ name: 'tammy' })
+            .set('Accept', 'application/json')
+            .then(response => {
+                expect(response.status).toBe(200);
+                console.timeEnd();
+            })
+    });
+})
+
+describe('post /', () => {
+    it('checking response type', () => {
+        return request(server)
+            .post('/api/persons')
+            .send({ name: 'turdly' })
+            .set('Accept', 'application/json')
+            .then(response => {
+                expect(response.type).toMatch(/json/i);
+            })
+
+    });
+})
--- persons/personModel.spec.js
+++ persons/personModel.spec.js
@@ -8,12 +8,13 @@ describe('person model in proper env', () => {
     })
 });
 
+
 describe('person model', () => {
     beforeEach(async() => {
         await db('person').truncate();
     });
 
-    describe('insert() then remove changes db length', () => {
+    describe('add() then  getById() then delete() changes db length', () => {
         it('should add a person into database', async() => {
             //start out with seeing if empty
             const data = await db('person');
@@ -44,4 +45,38 @@ describe('person model', () => {
         })
     })
 
+})
+
+describe('does getAll return an array', () => {
+    it('should return an array of seed persons', async() => {
+        //start out to check if truncate is in effect still
+        const people = await db('person');
+        expect(people).toHaveLength(0);
+        //truncate worked on this db so lets start over and add a couple
+
+        await Person.add({ name: 'turd' });
+        const newCount = await db('person');
+        expect(newCount).toHaveLength(1);
+        let all = await Person.getAll();
+        console.log(all);
+        //lets add another  and check again
+        await Person.add({ name: 'bacon' });
+        const newest = await db('person');
+        expect(newest).toHaveLength(2);
+        all = await Person.getAll();
+        console.log(all);
+        await Person.getAll()
+            .then(response => {
+                console.log(response);
+            })
+    })
+})
+
+describe('testing for info in seed data pre truncate below', () => {
+    it('checking for data', async() => {
+        await Person.getAll()
+            .then(response => {
+                console.log(response);
+            })
+    })
 })
